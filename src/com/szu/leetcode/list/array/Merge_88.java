63353ff4e01feaba1f2857b827e694fdbf50987e
XueyinGuo <guoxueyin@163.com>
2021-02-27 19:12:06 +0800

    88

---
+++ src/com/szu/leetcode/list/array/Merge_88.java
@@ -0,0 +1,35 @@
+package com.szu.leetcode.list.array;/*
+ * @Author 郭学胤
+ * @University 深圳大学
+ * @Description
+ * @Date 2021/2/22 21:24
+ */
+
+public class Merge_88 {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] res = new int[nums1.length];
+        System.arraycopy(nums1, 0, res, 0, nums1.length);
+        int index = 0;
+        int i1 = 0;
+        int i2 = 0;
+        while(i1 < m && i2 < n){
+
+            if(res[i1] >= nums2[i2]) nums1[index++] = nums2[i2++];
+            else nums1[index++] = res[i1++];
+
+
+        }
+
+        if(i1 < m){
+            for(int i = i1; i < m; i++)
+                nums1[index++] = res[i];
+        }
+
+        if(i2 < n) {
+            for (int i = i2; i < n; i++)
+                nums2[index++] = res[i];
+        }
+
+    }
+}
