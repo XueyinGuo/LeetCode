45c0ed6fe40c794148c04173ff4cf34e8a0c856d
XueyinGuo <guoxueyin@163.com>
2021-03-11 00:29:48 +0800

    mergeSort

--- src/com/szu/leetcode/sort/HeapSort.java
+++ src/com/szu/leetcode/sort/HeapSort.java
@@ -5,44 +5,78 @@ package com.szu.leetcode.sort;/*
  * @Date 2021/3/10 21:45
  */
 
+import java.util.Random;
+
 public class HeapSort {
-    static int heap[] = new int[16];
-    static int size = 0;
+    static int capacity = 16;
+    static int heap[] = new int[capacity];
+    static int size = -1;
 
     public static void main(String[] args) {
-        int[] arr = {};
-        heapInsert(arr,5);
-        pop();
+
+        int round = 20;
+        Random random = new Random();
+        for (int i = 0; i < round; i++)
+            push(random.nextInt(20));
+        for (int i = 0; i < round; i++)
+            System.out.print(pop() + " - ");
 
     }
 
     private static int pop() {
         int ans = heap[0];
-        swap(heap, 0, --size);
-        heapify(heap, 0, size);
+        swap(0, size--);
+        slipDown(0, size);
         return ans;
     }
 
-    private static void heapify(int[] heap, int index, int size) {
+    private static void swap(int i, int j) {
+        int temp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = temp;
+    }
+
+    private static void slipDown(int index, int size) {
         int left = index * 2 + 1;
         while (left < size){
-            int large = left + 1 > size ? left : ( heap[left+1] > heap[left] ? left + 1 : left );
-            
+            int smallIndex = getSmallIndex(left);
+            if (heap[smallIndex] == heap[index]) return;
+            swap(left, index);
+            index = smallIndex;
+            left = smallIndex * 2 + 1;
+        }
 
+    }
+
+    private static int getSmallIndex(int left) {
+        return left + 1 > size ? left : ( heap[left+1] < heap[left] ? left + 1 : left );
+    }
+
+    public static void push(int val){
+        size++;
+        if (size == capacity) grow();
+        heap[size] = val;
+        slipUp(size);
+
+    }
+
+    private static void grow() {
+        capacity = capacity << 1;
+        int[] newHeap = new int[capacity];
+        System.arraycopy(heap, 0, newHeap, 0, size);
+        heap = newHeap;
+    }
+
+    private static void slipUp(int index) {
+        int father = (index - 1) / 2;
+        while (heap[index] < heap[father]){
+            swap(index, father);
+            father = (father - 1) / 2;
+            index = (index - 1) / 2;
         }
     }
 
-    public void push(int val){
-
-        heapInsert(heap, val);
-        size++;
+    static int size(){
+        return size + 1;
     }
-
-    private static void heapInsert(int[] arr, int num) {
-        while(arr)
-
-    }
-
-
-
 }
