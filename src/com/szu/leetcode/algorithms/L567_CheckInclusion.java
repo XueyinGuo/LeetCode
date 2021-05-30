b72dbbedb49c9b5f720e8f39339483ff7c6c25c9
郭学胤 <54173572+XueyinGuo@users.noreply.github.com>
2021-05-30 20:24:36 +0800

    567

--- src/com/szu/leetcode/algorithms/L567_CheckInclusion.java
+++ src/com/szu/leetcode/algorithms/L567_CheckInclusion.java
@@ -18,65 +18,6 @@ import java.util.Map;
 
 public class L567_CheckInclusion {
 
-    public boolean checkInclusion(String s1, String s2) {
-        if (s1 == null || s1.length() == 0)
-            return true;
-        if (s2 == null || s2.length() == 0 || s2.length() < s1.length())
-            return false;
-        char[] str1 = s1.toCharArray();
-        HashMap<Character, Integer> map = new HashMap<>();
-        HashMap<Character, Integer> otherMap = new HashMap<>();
-        for (int i = 0; i < str1.length; i++) {
-            Integer times = map.get(str1[i]);
-            if (times != null)
-                map.put(str1[i], times + 1);
-            else
-                map.put(str1[i], 1);
-        }
-
-        char[] str2 = s2.toCharArray();
-
-        int i2 = 0;
-        while (i2 < str2.length) {
-            int helpIndex = i2;
-            while (helpIndex < str2.length && map.containsKey(str2[helpIndex])) {
-                Integer times = map.get(str2[helpIndex]);
-                Integer movedTimes = otherMap.get(str2[helpIndex]);
-                if (times == 1) {
-                    map.remove(str2[helpIndex]);
-                } else
-                    map.put(str2[helpIndex], times - 1);
-                if (movedTimes == null)
-                    otherMap.put(str2[helpIndex], 1);
-                else
-                    otherMap.put(str2[helpIndex], movedTimes + 1);
-                helpIndex++;
-            }
-            if (map.isEmpty())
-                return true;
-
-            moveBack(otherMap, map);
-            i2++;
-        }
-        return false;
-    }
-
-    private void moveBack(HashMap<Character, Integer> otherMap, HashMap<Character, Integer> map) {
-
-        Iterator<Map.Entry<Character, Integer>> iterator = otherMap.entrySet().iterator();
-        while (iterator.hasNext()) {
-            Map.Entry<Character, Integer> next = iterator.next();
-            Character key = next.getKey();
-            Integer movedTimes = next.getValue();
-
-            Integer times = map.get(key);
-            map.put(key, times == null ? movedTimes : times + movedTimes);
-
-        }
-        otherMap.clear();
-
-    }
-
 
     public static void main(String[] args) {
 
@@ -85,5 +26,37 @@ public class L567_CheckInclusion {
         L567_CheckInclusion test = new L567_CheckInclusion();
         boolean b = test.checkInclusion(s1, s2);
         System.out.println(b);
+    }
+
+    public boolean checkInclusion(String s1, String s2) {
+        /*
+         * 题目的意思就是在问，s1 的排列 是否是 s2的字串
+         * */
+        int[] map = new int[256];
+        char[] str1 = s1.toCharArray();
+        char[] str2 = s2.toCharArray();
+        for (int i = 0; i < str1.length; i++)
+            map[str1[i]]++;
+
+        int i = -1;
+        while (i < str2.length) {
+            int j = i + 1;
+            int all = str1.length;
+            int payMap[] = new int[256];
+            while (j < str2.length && map[str2[j]] != 0){
+                if (payMap[str2[j]] < map[str2[j]]){
+                    payMap[str2[j]]++;
+                    all--;
+                }else
+                    break;
+
+                if (all == 0)
+                    return true;
+
+                j++;
+            }
+            i = j;
+        }
+        return false;
     }
 }
